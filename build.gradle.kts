c8e1032a501170cbcfa17efa669e9a971bc30c6b
Creamoka <aryamaulanayusuf467@gmail.com>
2025-07-08 22:22:42 +0700

    Login & Session

--- build.gradle.kts
+++ build.gradle.kts
@@ -49,7 +49,7 @@ dependencies {
     implementation(platform("com.google.firebase:firebase-bom:33.16.0"))
     implementation("com.google.firebase:firebase-analytics")
     implementation("com.google.firebase:firebase-auth")
-    implementation("com.google.android.gms:play-services-auth")
+    implementation("com.google.android.gms:play-services-auth:20.7.0")
     implementation("com.google.firebase:firebase-firestore-ktx:24.7.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
 }
